fb0f043153579f9252c1b96e5af547a5b66e39c0
JI-NAMEUN <skadms1720@gmail.com>
2022-07-21 14:49:33 +0900

    구구단 jsp

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -1,26 +1,22 @@
 package com.ll.exam;
 
-import jakarta.servlet.ServletException;
+
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
+
 
 @WebServlet("/gugudan")
 public class GugudanServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Rq rq = new Rq(req, resp); // rq 도입
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        Rq rq = new Rq(req, resp);
 
         int dan = rq.getIntParam("dan", 9);
         int limit = rq.getIntParam("limit", 9);
-
         rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
-
-        resp.getWriter().append("<h1>%d단</h1>\n".formatted(dan));
-
         for (int i = 1; i <= limit; i++) {
             rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
         }
